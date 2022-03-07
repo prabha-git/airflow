3d54abd37557798d39b9eff2c63b98f7e6d7779a
prabha-git <prabhakaran.mails@gmail.com>
2022-03-07 14:00:30 +0000

    Basic error handling when no data for the date

--- dags/chicago_crime.py
+++ dags/chicago_crime.py
@@ -6,31 +6,35 @@ from airflow.operators.python_operator import PythonOperator
 from airflow import DAG
 from airflow.contrib.operators.bigquery_check_operator import BigQueryCheckOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.email_operator import EmailOperator
 
 import datetime as dt
+import pendulum
 
+
+local_tz = pendulum.timezone('America/Chicago')
 
 def run_etl(ds=None):
 	extract=Extract(dt.datetime.strptime(ds,'%Y-%m-%d'))
-	df = extract.execute_extraction()
-	print("Origina df\n\n",df.dtypes)
-	transform = Transformation(df)
-	transformed_df = transform.convert_datatype_datalake()
-	print("Transformed df\n\n",transformed_df.dtypes)
-	load = LoadToDataLake('af_data_lake','crime_data')
-	load.execute_load(transformed_df)
+        df = extract.execute_extraction()
+        if df:
+            transform = Transformation(df)
+            transformed_df = transform.convert_datatype_datalake()
+            load = LoadToDataLake('af_data_lake','crime_data')
+            load.execute_load(transformed_df)
 
 default_args = {
     'retries' : 0,
     'retry_delay' : dt.timedelta(minutes=1),
     'email_on_retry' : False,
     'email_on_failure' : False,
+    'email': ['prabhakaran.mails@gmail.com']
 }
 
 dag = DAG(
-    'chicago_crime_pipeline4',
+    'chicago_crime_pipeline5',
     default_args=default_args,
-    start_date=dt.datetime(2022,3,1),
+    start_date=dt.datetime(2022,3,1,tzinfo=local_tz),
     schedule_interval='0 0 * * *',
     catchup=True
 )
@@ -42,10 +46,20 @@ t1 = PythonOperator(
     dag=dag
 )
 
-t2 = BashOperator(
-    task_id='count_line',
-    bash_command='wc -l ~/prabha.txt >> ~/prabha_count.txt',
-    dag=dag
+t2 = BigQueryCheckOperator(
+    task_id="check_etl_completion",
+    sql=f"SELECT COUNT(*) FROM airflow-341215.af_data_lake.crime_data",
+    use_legacy_sql=False
 )
 
-t1 >> t2
+
+email = EmailOperator(
+        task_id='send_email',
+        to='prabhakaran.mails@gmail.com',
+        subject='Airflow Alert',
+        html_content=""" <h3>Airflow Pipeline Complete !!! t</h3> """,
+        dag=dag
+)
+
+
+t1 >> t2 >> email
