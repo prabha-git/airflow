d4120259d0b6e78fe5d05763cd94dd485f934328
prabha-git <prabhakaran.mails@gmail.com>
2022-03-26 23:42:25 -0500

    Updated the warehouse load sql to point the airflow project

--- dags/warehouse_load.sql
+++ dags/warehouse_load.sql
@@ -1,17 +1,17 @@
 DECLARE fields STRING;
 DECLARE updates STRING;
 EXECUTE IMMEDIATE (
-     "SELECT STRING_AGG(column_name) FROM `chicago-crime3.warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr'"
+     "SELECT STRING_AGG(column_name) FROM `airflow-341215.af_data_warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr'"
   ) INTO fields;
 EXECUTE IMMEDIATE (
-    """WITH t AS (SELECT column_name FROM `chicago-crime3.warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr')
+    """WITH t AS (SELECT column_name FROM `airflow-341215.af_data_warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr')
        SELECT STRING_AGG("t."||column_name ||" = "|| "s."||column_name) from t join t as s using(column_name)"""
   ) INTO updates;
 
 
   EXECUTE IMMEDIATE """
-  MERGE `chicago-crime3.warehouse.crime_data_curr` T
-  USING `chicago-crime3.data_lake.crime_data` S
+  MERGE `airflow-341215.af_data_warehouse.crime_data_curr` T
+  USING `airflow-341215.af_data_lake.crime_data` S
     ON T.id = S.id
   WHEN MATCHED and s.updated_on > t.updated_on THEN 
     UPDATE SET """||updates||"""
@@ -26,10 +26,10 @@ DECLARE fields_hist STRING;
 DECLARE updates STRING;
 DECLARE fields_hist_value STRING;
 EXECUTE IMMEDIATE (
-     "SELECT STRING_AGG(column_name) FROM `chicago-crime3.warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr'"
+     "SELECT STRING_AGG(column_name) FROM `airflow-341215.af_data_warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr'"
   ) INTO fields;
 EXECUTE IMMEDIATE (
-    """WITH t AS (SELECT column_name FROM `chicago-crime3.warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr')
+    """WITH t AS (SELECT column_name FROM `airflow-341215.af_data_warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr')
        SELECT STRING_AGG("t."||column_name ||" = "|| "s."||column_name) from t join t as s using(column_name)"""
   ) INTO updates;
 
@@ -37,8 +37,8 @@ set fields_hist = concat(fields,",is_active,record_expr_date");
 
 set fields_hist_value = concat(fields,",True,'2099-12-31'");
   EXECUTE IMMEDIATE """
-  MERGE `chicago-crime3.warehouse.crime_data_hist` T
-  USING `chicago-crime3.warehouse.crime_data_curr` S
+  MERGE `airflow-341215.af_data_warehouse.crime_data_hist` T
+  USING `airflow-341215.af_data_warehouse.crime_data_curr` S
     ON T.id = S.id
   WHEN MATCHED and s.updated_on > t.updated_on THEN 
     UPDATE SET record_expr_date = DATETIME_SUB(S.updated_on,INTERVAL 1 SECOND),
@@ -52,10 +52,10 @@ DECLARE fields_hist STRING;
 DECLARE updates STRING;
 DECLARE fields_hist_value STRING;
 EXECUTE IMMEDIATE (
-     "SELECT STRING_AGG(column_name) FROM `chicago-crime3.warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr'"
+     "SELECT STRING_AGG(column_name) FROM `airflow-341215.af_data_warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr'"
   ) INTO fields;
 EXECUTE IMMEDIATE (
-    """WITH t AS (SELECT column_name FROM `chicago-crime3.warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr')
+    """WITH t AS (SELECT column_name FROM `airflow-341215.af_data_warehouse`.INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'crime_data_curr')
        SELECT STRING_AGG("t."||column_name ||" = "|| "s."||column_name) from t join t as s using(column_name)"""
   ) INTO updates;
 
@@ -63,8 +63,8 @@ set fields_hist = concat(fields,",is_active,record_expr_date");
 
 set fields_hist_value = concat(fields,",True,'2099-12-31'");
   EXECUTE IMMEDIATE """
-  MERGE `chicago-crime3.warehouse.crime_data_hist` T
-  USING `chicago-crime3.warehouse.crime_data_curr` S
+  MERGE `airflow-341215.af_data_warehouse.crime_data_hist` T
+  USING `airflow-341215.af_data_warehouse.crime_data_curr` S
     ON T.id = S.id and T.updated_on = S.updated_on
   WHEN NOT MATCHED THEN
     INSERT ("""||fields_hist||""") VALUES ("""||fields_hist_value||""")"""
